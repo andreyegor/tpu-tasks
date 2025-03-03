5779063ecea90cd787c914e880206cc2c71d88cc
Egor <andreevegor10000@gmail.com>
2025-03-03 22:59:32 +0700

    Informatics lab-3 Solution

---
+++ informatics/lab-3/lab-3/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxX = new TextBox();
+            textBoxQ = new TextBox();
+            textBoxAns = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            buttonRun = new Button();
+            buttonClear = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            groupBox1 = new GroupBox();
+            radioButton3 = new RadioButton();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBoxX
+            // 
+            textBoxX.Location = new Point(164, 36);
+            textBoxX.Name = "textBoxX";
+            textBoxX.Size = new Size(150, 31);
+            textBoxX.TabIndex = 0;
+            // 
+            // textBoxQ
+            // 
+            textBoxQ.Location = new Point(164, 73);
+            textBoxQ.Name = "textBoxQ";
+            textBoxQ.Size = new Size(150, 31);
+            textBoxQ.TabIndex = 1;
+            // 
+            // textBoxAns
+            // 
+            textBoxAns.Location = new Point(56, 285);
+            textBoxAns.Multiline = true;
+            textBoxAns.Name = "textBoxAns";
+            textBoxAns.Size = new Size(258, 221);
+            textBoxAns.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Введите X";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(56, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Введите Q";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(56, 257);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 5;
+            label3.Text = "label3";
+            // 
+            // buttonRun
+            // 
+            buttonRun.Location = new Point(56, 512);
+            buttonRun.Name = "buttonRun";
+            buttonRun.Size = new Size(112, 34);
+            buttonRun.TabIndex = 6;
+            buttonRun.Text = "Пуск";
+            buttonRun.UseVisualStyleBackColor = true;
+            buttonRun.Click += buttonRun_Click;
+            // 
+            // buttonClear
+            // 
+            buttonClear.Location = new Point(202, 512);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(112, 34);
+            buttonClear.TabIndex = 7;
+            buttonClear.Text = "Отчистить";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 30);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(73, 29);
+            radioButton1.TabIndex = 8;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "sh(x)";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(6, 65);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(67, 29);
+            radioButton2.TabIndex = 9;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "x^2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Location = new Point(56, 110);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(258, 141);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Выберите f(x)";
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(6, 100);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(66, 29);
+            radioButton3.TabIndex = 10;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "e^x";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(368, 659);
+            Controls.Add(groupBox1);
+            Controls.Add(buttonClear);
+            Controls.Add(buttonRun);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxAns);
+            Controls.Add(textBoxQ);
+            Controls.Add(textBoxX);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxX;
+        private TextBox textBoxQ;
+        private TextBox textBoxAns;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button buttonRun;
+        private Button buttonClear;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+    }
+}
---
+++ informatics/lab-3/lab-3/Form1.cs
@@ -0,0 +1,77 @@
+using System.Xml;
+
+namespace lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBoxX.Text, out double x) &&
+            double.TryParse(textBoxQ.Text, out double q))
+            {
+                double fx = -1;
+                string fx_text = "";
+                if (radioButton1.Checked)
+                {
+                    fx = Math.Sinh(x);
+                    fx_text = "sh(x)";
+                }
+                else if (radioButton2.Checked)
+                {
+                    fx = x * x;
+                    fx_text = "x^2";
+                }
+                else if (radioButton3.Checked)
+                {
+                    fx = Math.Exp(x);
+                    fx_text = "e^x";
+                }
+                else
+                {
+                    textBoxAns.Text = "Выберите f(x)";
+                    return;
+                }
+
+                double xq = Math.Abs(x * q);
+                double ans = -1;
+                if (xq > 10)
+                {
+                    ans = Math.Log(Math.Abs(fx) * Math.Abs(q));
+                }
+                else if (xq == 10)
+                {
+                    ans = Math.Exp(fx + q);
+                }
+                else
+                {
+                    ans = fx + q;
+                }
+
+                string res_template =
+                    "Результат работы программы Андреева \r\n" +
+                    "При x = {0}\r\nПри q={1}\r\nПри f(x) = {2}\r\n" +
+                    "k = {3}";
+                textBoxAns.Text = string.Format(res_template, x, q, fx_text, ans);
+            }
+            else 
+            {
+                textBoxAns.Text = "Ошибка";
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxAns.Clear();
+        }
+    }
+}

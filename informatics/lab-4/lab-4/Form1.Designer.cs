5a3df942ae9eac3f3f3f0ef83e8d28c8f8239075
Egor <andreevegor10000@gmail.com>
2025-03-03 23:51:12 +0700

    Informatics lab-4

---
+++ informatics/lab-4/lab-4/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(48, 15);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(48, 52);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 25);
+            label1.TabIndex = 2;
+            label1.Text = "x0";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 25);
+            label2.TabIndex = 3;
+            label2.Text = "xk";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 25);
+            label3.TabIndex = 5;
+            label3.Text = "dx";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(48, 89);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 31);
+            textBox3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 129);
+            label4.Name = "label4";
+            label4.Size = new Size(23, 25);
+            label4.TabIndex = 7;
+            label4.Text = "b";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(48, 126);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(150, 31);
+            textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(12, 163);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.ScrollBars = ScrollBars.Both;
+            textBox1.WordWrap = false;
+            textBox5.Size = new Size(615, 168);
+            textBox5.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(48, 337);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 9;
+            button1.Text = "Вычислить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(657, 391);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button1;
+    }
+}
---
+++ informatics/lab-4/lab-4/Form1.cs
@@ -0,0 +1,39 @@
+namespace lab_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "-2,05";
+            textBox2.Text = "-3,05";
+            textBox3.Text = "-0,2";
+            textBox4.Text = "3,4";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox5.Clear();
+            float x0, xk, dx, b;
+            if (!float.TryParse(textBox1.Text, out x0) ||
+                !float.TryParse(textBox2.Text, out xk) ||
+                !float.TryParse(textBox3.Text, out dx) ||
+                !float.TryParse(textBox4.Text, out b))
+            {
+                textBox5.Text = "Ошибка ввода!";
+                return;
+            }
+
+            textBox5.AppendText("Выполнил Андреев Е\r\n");
+            for (float x = x0; Math.Abs(x) <= Math.Abs(xk); x = x + dx) {
+                double y = x * Math.Sin(Math.Sqrt(x+b-0.0084));
+                string ans = string.Format("x={0} y={1}\r\n",x,y);
+                textBox5.AppendText(ans);
+            }
+        }
+    }
+}

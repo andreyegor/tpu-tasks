d71ed1db10dbac6e21795d7cd1f532b9e3f3d6ac
Egor <andreevegor10000@gmail.com>
2025-03-05 21:59:29 +0700

    h&w swapped

--- lab-1/main.java
+++ lab-1/main.java
@@ -1,15 +1,15 @@
 class Main {
     public static void main(String[] args) {
         System.out.printf("Мой вариант: %d\n", 108 % 4);
-        int width = 10, height = 5;
+        int height = 5, width = 10;
         assert 1 <= width && 1 <= height && width <= 100 && height <= 100;
 
         var out = new StringBuilder();
         String template = "%%%dd * %%%dd = %%%dd %%s".formatted(String.valueOf(height).length(),
                 String.valueOf(width).length(), String.valueOf(width * height).length());
-        for (int w = 1; w <= height; w++)
-            for (int h = 1; h <= width; h++) {
-                out.append(template.formatted(w, h, h * w, h == width ? "\n" : "    "));
+        for (int h = 1; h <= height; h++)
+            for (int w = 1; w <= width; w++) {
+                out.append(template.formatted(h, w, h * w, w == width ? "\n" : "    "));
             }
 
         System.out.print(out);

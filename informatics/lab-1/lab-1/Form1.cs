4f260e6ea83daf00d8e3d446b6a55819c2627aaa
Egor <andreevegor10000@gmail.com>
2025-03-05 19:59:46 +0700

    informatics lab-5

--- informatics/lab-1/lab-1/Form1.cs
+++ informatics/lab-1/lab-1/Form1.cs
@@ -9,12 +9,14 @@ namespace lab_1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            textBox1.AppendText("����� � ������");
+            textBox1.AppendText("Готов к работе");
         }
 
         //click event для всех кнопок
         private void anyButton_Click(object sender, EventArgs e) {
             //пробуем корректно привести object к button
+            if (!(sender is Button))
+                return;
             var btn = sender as Button;
             if (btn == null)
                 return;
---
+++ informatics/lab-5/lab-5/Form1.cs
@@ -0,0 +1,39 @@
+namespace lab_5
+{
+    public partial class Form1 : Form
+    {
+        private List<Panel> panels = [];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int side = 100;
+            int panelid = panels.Count();
+            var that_panel = new Panel();
+            that_panel.Size = new Size(side, side);
+            that_panel.Location = new Point(side * panels.Count(), 0);
+            that_panel.Name = "panel" + panelid.ToString();
+            that_panel.BackColor = Color.DarkCyan;
+            this.Controls.Add(that_panel);
+            panels.Add(that_panel);
+            Console.WriteLine(panels.Count());
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var panel in panels)
+            {
+                if (!panel.Controls.OfType<TextBox>().Any())
+                {
+                    var that_textBox = new TextBox();
+                    that_textBox.Text = panel.Name;
+                    panel.Controls.Add(that_textBox);
+                }
+            }
+        }
+    }
+}

902ad68e0c0e3a0289dade0e07f8b90f560014fd
Egor <andreevegor10000@gmail.com>
2025-03-14 13:36:37 +0700

    inf lab-13

---
+++ informatics/lab-13/lab-13/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace lab_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int n in Enumerable.Range(-10, 21).Select(f)) {
+                Console.Write(n);
+                Console.Write(' ');
+            }
+        }
+
+        private static int f(int n) {
+            return n >= 0 ? n * n : n * n * n;
+        }
+    }
+}

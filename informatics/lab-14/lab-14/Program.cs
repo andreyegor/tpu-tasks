1e296f9bb1b165075d451350be80dc7be7dce595
Egor <andreevegor10000@gmail.com>
2025-03-22 19:20:07 +0700

    lab 14 fix

--- informatics/lab-14/lab-14/Program.cs
+++ informatics/lab-14/lab-14/Program.cs
@@ -1,12 +1,15 @@
 ﻿namespace lab_14
 {
+    //Не создавать новый массив для сортировки;
+    //Ввод числа для поиска
+    //реализовать простой поиск
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Мой вариант: {0}", 10 % 3 + 1);//+1 тк индексация c 1
 
-            int ln = 100;
+            int ln = 10;
 
             var r = new Random();
             var m = new int[ln];
@@ -14,46 +17,51 @@
                 m[i] = r.Next(-100, 100);
 
             WriteArray(m);
-            var ms = InsertionSort(m);
-            WriteArray(ms);
-            Console.WriteLine("Глубина рекурсии в бинарного(методом дитхомии) поиска: {0}", BinSearch(ms, m[0]));
+            InsertionSort(m);
+            WriteArray(m);
+            Console.Write("Введите число для поиска: ");
+            if (!int.TryParse(Console.ReadLine(), out int target)) {
+                Console.WriteLine("Это не число.");
+                return;
+            }
+            var (s, sd) = Search(m, target);
+            var (bs, bsd) = BinSearch(m, target);
+            Func<int, string> foundNotFound = i => i == -1 ? "элемент не найден" : String.Format("элемент найден на {0} месте", i);
+            Console.WriteLine("В результате обычного поиска по отсортированному массиву {0}, а в результате бинарного (методом дитхомии) {1}.", foundNotFound(s), foundNotFound(bs));
+            Console.WriteLine("Элмент был (не)найден обычным поиском за {0} итераций, а бинарным (методом дитхомии) за {1} итераций.", sd, bsd);
 
         }
 
-        private static int[] InsertionSort(int[] m)
+        private static void InsertionSort(int[] m)
         {
-            var mc = new int[m.Length];
-            m.CopyTo(mc, 0);
-
-            for (int i = 1; i < mc.Length; i++)
+            for (int i = 1; i < m.Length; i++)
             {
-                int key = mc[i];
+                int key = m[i];
                 int j;
 
-                for (j = i - 1; j >= 0 && mc[j] > mc[i]; j--)
-                    mc[j + 1] = mc[j];
+                for (j = i - 1; j >= 0 && m[j] > key; j--)
+                    m[j + 1] = m[j];
 
 
-                mc[j + 1] = key;
+                m[j + 1] = key;
             }
-            return mc;
         }
 
 
         // Пишем очень императивно, не вижу причин не раскрывать рекурсию
         // Индекс элемента не нужен, по этому возвращаю число итераций поиска (глубину рекурсии)
-        private static int BinSearch(int[] m, int target)
+        private static (int, int) BinSearch(int[] m, int target)
         {
             int l = 0, r = m.Length - 1;
-            int i = 0;
+            int depth = 1;
 
             while (l <= r)
             {
-                i++;
-                int mid = l + (r - l) / 2;
+                depth++;
+                int mid = (r + l) / 2;
                 if (m[mid] == target)
                 {
-                    return i;
+                    return (mid, depth);
                 }
                 else if (m[mid] < target)
                 {
@@ -65,7 +73,15 @@
                 }
             }
 
-            return i; // Target not found
+            return (-1, depth);
+        }
+
+        private static (int, int) Search(int[] m, int target)
+        {
+            for (int i = 0; i < m.Length; i++)
+                if (m[i] == target)
+                    return (i, i+1);
+            return (-1, m.Length); // Target not found
         }
 
         private static void WriteArray(int[] m)

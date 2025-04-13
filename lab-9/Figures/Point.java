f34c06555951fc7d0ab7fec75870f41e2a91c5d1
Egor <andreevegor10000@gmail.com>
2025-04-13 15:59:53 +0700

    init

---
+++ lab-9/Figures/Point.java
@@ -0,0 +1,31 @@
+package Figures;
+
+import java.util.Objects;
+
+public class Point implements Figure {
+    private final Vector Oxy;
+
+    public Point(Vector pos) {
+        this.Oxy = pos;
+    }
+
+    @Override
+    public void draw() {
+    };
+
+    public Point move(Vector to) {
+        return new Point(Oxy.add(to));
+    }
+
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+        if (other == null || getClass() != other.getClass())
+            return false;
+        return ((Point) other).Oxy == Oxy;
+    }
+
+    public int hashCode() {
+        return Objects.hash(Oxy);
+    }
+}

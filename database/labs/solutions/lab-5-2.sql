fd78561b524ced4e3387a869336e929a08f305e7
Egor <andreevegor10000@gmail.com>
2026-03-13 13:42:46 +0700

    lab-5-2

---
+++ database/labs/solutions/lab-5-2.sql
@@ -0,0 +1,70 @@
+-- Для каждого отдела отобразите номер отдела, название отдела, регион и максимальную ЗП в отделе, которая больше 12000.
+SELECT
+  ds.department_id,
+  ds.department_name,
+  max(es.salary) as max_salary
+FROM
+  hr.departments ds
+  JOIN hr.employees es ON es.department_id = ds.department_id
+GROUP BY
+  ds.department_id,
+  ds.department_name
+HAVING
+  max(es.salary) > 12000;
+-- Получите список всех отделов и сотрудников, включая отделы без сотрудников (используйте команду USING при соединении таблиц).
+SELECT
+  ds.department_name,
+  CASE
+    WHEN es.employee_id IS NOT NULL THEN format('%s %s', es.first_name, es.last_name)
+    ELSE '-'
+  END as full_name
+FROM
+  hr.departments ds
+  LEFT JOIN hr.employees es USING(department_id);
+-- Для каждого отдела отобразите номер отдела, название отдела, регион и минимальную ЗП в отделе и список сотрудников в виде "Фамилия И." разделенных через запятую.
+SELECT
+  ds.department_id,
+  ds.department_name,
+  rs.region_name,
+  min(es.salary) as min_salary,
+  string_agg(
+    format(
+      '%s %s.',
+      es.first_name,
+      substr(es.last_name, 1, 1)
+    ),
+    ', '
+  ) as employees
+FROM
+  hr.departments ds
+  JOIN hr.employees es ON es.department_id = ds.department_id
+  JOIN hr.locations ls ON ls.location_id = ds.location_id
+  JOIN hr.countries cs ON cs.country_id = ls.country_id
+  JOIN hr.regions rs ON rs.region_id = cs.region_id
+GROUP BY
+  ds.department_id,
+  ds.department_name,
+  rs.region_name;
+-- Выведите регионы, в которых нет ни одного отдела.
+SELECT
+  rs.region_name
+FROM
+  hr.regions rs
+  LEFT JOIN hr.countries cs ON cs.region_id = rs.region_id
+  LEFT JOIN hr.locations ls ON ls.country_id = cs.country_id
+  LEFT JOIN hr.departments ds ON ds.location_id = ls.location_id
+GROUP BY
+  rs.region_name
+HAVING
+  count(ds.department_name) = 0;
+-- Придумайте самостоятельно запрос, с внешним соединением. Запрос должн быть основаны минимум на 3-х таблицах и включать различные условия.
+SELECT
+  format('%s %s', es.first_name, es.last_name),
+  ds.department_name,
+  ls.city
+FROM
+  hr.employees es
+  JOIN hr.departments ds ON ds.department_id = es.department_id
+  JOIN hr.locations ls ON ls.location_id = ds.location_id
+WHERE
+  lower(substr(ls.city, 1, 1)) != 's';
